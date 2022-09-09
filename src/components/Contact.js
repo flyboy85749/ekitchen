f2ec7134711c4216f039bedafafbf9ec86fb8ef7
flyboy85749 <freedmouse@gmail.com>
2022-09-09 11:22:21 -0700

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_tlfiywo', 'template_ounlgv9', form.current, 'mnbNxVy4nLQ5lCRCz')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
   return (
-    <div>We'll have a contact form here at some point.</div>
+    <section>
+      <div classNameName="container">
+        <h2>Contact Us</h2>
+        <form action={sendEmail} ref={form} className="w-full max-w-sm">
+  <div className="md:flex md:items-center mb-6">
+    <div className="md:w-1/3">
+      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="user_name">
+        Full Name
+      </label>
+    </div>
+    <div className="md:w-2/3">
+      <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" value="" />
+    </div>
+  </div>
+  <div className="md:flex md:items-center mb-6">
+    <div className="md:w-1/3">
+      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="user_email">
+        Email
+      </label>
+    </div>
+    <div className="md:w-2/3">
+      <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="email" placeholder="" />
+    </div>
+  </div>
+  <div className="md:flex md:items-center mb-6">
+    <div className="md:w-1/3"></div>
+    
+  </div>
+  <div className="md:flex md:items-center">
+    <div className="md:w-1/3"></div>
+    <div className="md:w-2/3">
+      <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+        Send Email
+      </button>
+    </div>
+  </div>
+</form>
+      </div>
+    </section>
   )
 }
 
